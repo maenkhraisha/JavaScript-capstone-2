c744f3b9d01edca20e13f453b92dcfca5c989ea6
ekenecf <ekens2u@gmail.com>
2022-04-26 18:11:45 +0100

    Implement tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import '../style.css';
-// import { commentCounter } from './Module/counters.js';
 import displayPopup from './Module/display_popup.js';
 import displayReservation from './Module/display_popup_reservation.js';
 import display from './Module/display_home.js';
@@ -7,10 +6,6 @@ import { itemCounter } from './Module/counters.js';
 /* eslint-disable no-unused-vars */
 
 /* eslint-disable no-use-before-define */
-const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-const urlID = '7amfNDyGsWWSaz3PJUCx';
-const url2 = 'https://themealdb.com/api/json/v1/1/categories.php';
-const show = document.querySelector('.display');
 display();
 displayPopup();
 displayReservation();
