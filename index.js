443e50412b8847fcf0bac3ca3b1c4496bdd6627e
maen <maenkh@yahoo.com>
2022-04-27 05:08:00 +0300

    edit javascript location to root folder

---
+++ index.js
@@ -0,0 +1,12 @@
+import './style.css';
+import displayPopup from './src/Module/display_popup.js';
+import displayReservation from './src/Module/display_popup_reservation.js';
+import display from './src/Module/display_home.js';
+import { itemCounter } from './src/Module/counters.js';
+/* eslint-disable no-unused-vars */
+
+/* eslint-disable no-use-before-define */
+display();
+displayPopup();
+displayReservation();
+itemCounter();
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './index.js',
   devServer: {
     static: './dist',
   },
