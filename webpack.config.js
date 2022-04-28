025d76610bbaeaf3eae304b829d55958e0de730f
collinsmezie <collinschimezieobinwa@gmail.com>
2022-04-28 18:36:07 +0400

    Deploy the app

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: "JavaScript-capstone-2",
   },
   devServer: {
     static: './dist',
